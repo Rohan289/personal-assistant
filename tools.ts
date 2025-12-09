c384cf37605060911899a808d4f9ae2d5a8504f5
Rohan Bhowmick <rohanbhowmick@Rohans-MacBook-Air.local>
2025-12-09 10:37:55 +0530

    Added secrets to env file

--- tools.ts
+++ tools.ts
@@ -9,7 +9,10 @@ const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_SECRET,
     process.env.GOOGLE_REDIRECT_URL
   );
-  oauth2Client.setCredentials(tokens);
+  oauth2Client.setCredentials({
+    access_token : process.env.GOOGLE_ACCESS_TOKEN,
+    refresh_token : process.env.GOOGLE_REFRESH_TOKEN,
+  });
 
 const calendar = google.calendar({version: 'v3', auth : oauth2Client});
 type EventParams = {
