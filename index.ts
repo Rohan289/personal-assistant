3810d789659428bb5ff1fceb6d4bab8c79a8ca35
Rohan Bhowmick <rohanbhowmick@Rohans-MacBook-Air.local>
2025-12-09 10:11:47 +0530

    Added terminal UI and memory to the agent

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 
 import { ChatGroq } from "@langchain/groq";
 import { createEventTool, getEventsTool } from "./tools";
-import { StateGraph,  MessagesAnnotation, END } from "@langchain/langgraph";
+import { StateGraph,  MessagesAnnotation, END, MemorySaver } from "@langchain/langgraph";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import type { AIMessage } from "@langchain/core/messages";
+import readline from "node:readline/promises";
+
+const memory = new MemorySaver();
 
 const tools = [createEventTool,getEventsTool];
 const model = new ChatGroq({
@@ -34,16 +37,25 @@ const graph = new StateGraph(MessagesAnnotation).addNode('assistant',callModel).
     tools : 'tools'
 });
 
-const app = graph.compile();
+const app = graph.compile({checkpointer : memory});
 
 async function main() {
-    const result = await app.invoke({
-        messages : [
-            // { role : 'user', content : 'Do I have any meeting today?' }
-            {role : 'user',content : 'Can you create a meeting with Archi(senarchi4@gmail.com) at 7pm IST today about Design discussion?'}
-        ]
-    });
-    console.log("AI : ",result.messages[result.messages.length -1]?.content);
+    const rlInterface = readline.createInterface({input : process.stdin,output : process.stdout});
+    let config = {configurable : {thread_id : '1'}};
+    while(true) {
+        const userInput = await rlInterface.question("You: ");
+        if (userInput.toLowerCase() === 'bye' || userInput.toLowerCase() === 'quit') {
+            console.log("Exiting...");
+            break;
+        }
+        const result = await app.invoke({
+            messages : [
+                { role : 'user', content : userInput }
+            ]
+        },config);
+        console.log("AI : ",result.messages[result.messages.length -1]?.content);
+    }
+    rlInterface.close();
 }
 
 main();
